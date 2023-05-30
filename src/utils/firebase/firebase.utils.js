68bfa4320a22df0d9ebccc0e53fe97011edfd694
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-05-29 23:18:29 -0500

    More sign in stuff.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMdOwpZIjjLRdERqLa9Rrtj3BJ-_eTyn0",
+  authDomain: "crwn-clothing-db-dd798.firebaseapp.com",
+  projectId: "crwn-clothing-db-dd798",
+  storageBucket: "crwn-clothing-db-dd798.appspot.com",
+  messagingSenderId: "348171440596",
+  appId: "1:348171440596:web:b042cac17d42c62d9a0641"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
