4d4699d56de806095665bb16365ef8de03ea4acf
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-06-01 23:59:55 -0500

    Created barebones SignUpForm component.

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    setFormFields({
+      ...formFields,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <div>
+      <h1>Sign up with your e-mail and password.</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+        <label>E-mail address</label>
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
