e90f97682a200e9d807eb1d9ebea66618575da34
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-06-10 00:17:12 -0500

    Counting items in cart
    Also moved some things around... fun!

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,16 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+import { UserContext } from "../../contexts/user.context";
+
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const NavBar = () => {
   const { currentUser } = useContext(UserContext);
