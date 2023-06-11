7f963972f253f44350d6344aff81e3f1a889f257
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-06-10 22:18:04 -0500

    Added cart total

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -42,12 +42,14 @@ export const CartContext = createContext({
   clearItemFromCart: () => {},
   removeItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     const newCartCount = cartItems.reduce(
@@ -56,6 +58,14 @@ export const CartProvider = ({ children }) => {
     );
     setCartCount(newCartCount);
   }, [cartItems]);
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + (cartItem.quantity * cartItem.price),
+      0
+    );
+    setCartTotal(newCartTotal);
+  });
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
@@ -77,6 +87,7 @@ export const CartProvider = ({ children }) => {
     removeItemFromCart,
     cartItems,
     cartCount,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
