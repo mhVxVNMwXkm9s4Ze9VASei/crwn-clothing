63199f9cf1cd61434e70cdbdae85299515396954
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-06-13 22:32:21 -0500

    Created Button styled component
    Naisu.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 import {
   signInWithGooglePopup,
@@ -92,7 +92,7 @@ const SignInForm = () => {
           <Button type="submit">Sign In</Button>
           <Button
             type="button"
-            buttonType="google"
+            buttonType={BUTTON_TYPE_CLASSES.google}
             onClick={signInWithGooglePopup}
           >
             Google Sign In
