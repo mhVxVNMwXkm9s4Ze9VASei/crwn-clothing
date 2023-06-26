dd40864024cbd39956a2ed668194c43ee52aa59a
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-06-25 22:07:11 -0500

    User Reducer stuff
    Apparently reducers work a bit differently now... I'll come back and change everything after I go through the course so I don't confuse myself.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth
+} from "./utils/firebase/firebase.utils";
+
 import Authentication from "./routes/authentication/authentication.component";
 import Checkout from "./routes/checkout/checkout.component";
 import Home from "./routes/home/home.component";
 import NavBar from "./routes/navigation/navigation.component";
 import Shop from "./routes/shop/shop.component";
+import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(
+    () => {
+      const unsubscribe = onAuthStateChangedListener((user) => {
+        if (user) {
+          createUserDocumentFromAuth(user);
+        }
+
+        dispatch(setCurrentUser(user));
+      });
+
+      return unsubscribe;
+    },
+    [dispatch]
+  );
+
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
